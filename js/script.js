e0dad5412cb778e94ecbfa1d436206fb6a81c4ff
Karina <almeida.karinait@gmail.com>
2023-10-22 01:27:36 -0300

    se agregan productos

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ let menu_bar = document.querySelector('#menu-bar')
 menu_bar.addEventListener('click', () => {
     menu.classList.toggle('menu-toggle')
 })
+//se agrega seleccionador de cantidad de productos
+const cantidad = document.getElementById("cantidad");
+const buttonMas = document.getElementById("button+");
+const buttonMenos = document.getElementById("button-");
+let items = 0;
+function aumentarItem() {
+    items++;
+    cantidad.textContent = items;
+}
+function restarItem() {
+    if(items > 0) items--;
+    cantidad.textContent = items;
+}
+buttonMas.addEventListener("click", aumentarItem);
+buttonMenos.addEventListener("click", restarItem);
+
